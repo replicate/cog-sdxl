a2b0c206eb9168d608e32b0991b662836dc7aedb
Dan Nelson <dan@replicate.com>
2023-09-27 16:34:36 -0700

    User Provided Captions & Gardening (#30)
    - Adds the ability for users to provide their own captions when training
    SDXL by uploading a `captions.csv` within their images zip/tar which
    contains captions (under the column heading `caption`) and images (under
    the column heading `image_file`). Should address #29
    - Shifts preprocessing weight storage to Replicate
    - Fixes #26

--- predict.py
+++ predict.py
@@ -68,6 +68,8 @@ def download_weights(url, dest):
 
 class Predictor(BasePredictor):
     def load_trained_weights(self, weights, pipe):
+        # weights can be a URLPath, which behaves in unexpected ways
+        weights = str(weights)
         if self.tuned_weights == weights:
             print("skipping loading .. weights already loaded")
             return
@@ -158,6 +160,8 @@ class Predictor(BasePredictor):
         start = time.time()
         self.tuned_model = False
         self.tuned_weights = None
+        if str(weights) == 'weights':
+            weights = None
 
         self.weights_cache = WeightsDownloadCache()
 
